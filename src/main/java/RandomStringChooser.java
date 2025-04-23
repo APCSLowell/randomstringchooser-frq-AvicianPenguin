a0ebff7e6ffb5ad0573a88de19f7648711843e8a
Penguin <aleksandervolkov2007@gmail.com>
2025-04-23 09:16:19 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,28 +1,21 @@
 import java.util.*;
 public class RandomStringChooser
 {
-  private String [] wordArray;
-  public RandomStringChooser(String[] arr){
-    wordArray = arr;
+  private List <String> wordArray;
+  public RandomStringChooser(String[] arr)
+  {
+    wordArray = new <String>();
+    for(word:arr)
+      wordArray.add(word);
   }
-  public String getNext(){
-    if(wordArray.length<1)
+  public String getNext()
+  {
+    if(wordArray.size()<1)
       return ("NONE");
-    int count = 0;
-    int randomIndex = (int)(Math.random()*(wordArray.length));
-    String value = wordArray[randomIndex];
-    String [] newWordArray = new String [wordArray.length-1];
-    for(int i = 0; i<wordArray.length; i++)
-      {
-        if(!wordArray[i].equals(value))
-          newWordArray[i] = wordArray[i];
-        else
-        {
-          count++;
-          newWordArray[i] = wordArray[i+count];
-        }
-      }
-    wordArray = newWordArray; 
-    return value;
+    else
+    {
+      int value = (int)(Math.random()*(wordArray.length));
+      return(wordArray.remove(value));
+    }
   }
 }
