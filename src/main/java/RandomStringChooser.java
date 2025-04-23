68f2043907c11f9a63fa84d5734495a413852c21
Penguin <aleksandervolkov2007@gmail.com>
2025-04-23 09:03:21 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -5,4 +5,24 @@ public class RandomStringChooser
   
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
+  private String [] wordArray;
+  public RandomStringChooser(String[] arr){
+    wordArray = arr;
+  }
+  public String getNext(){
+    if(wordArray.length<1)
+      return ("NONE")
+    int randomIndex = (int)(Math.random()*(wordArray.length));
+    String value = wordArray[randomIndex];
+    String [] newWordArray = new String [wordArray.length-1];
+    for(int i = 0; i<wordArray.length; i++)
+      {
+        if(!wordArray[i].equals(value))
+          newWordArray[i] = wordArray[i];
+        else
+          newWordArray[i-1] = wordArray[i];
+      }
+    wordArray = newWordArray; 
+    return value;
+  }
 }
